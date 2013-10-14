8eb67c76da6198c0babebe071941c439a0a9c3a9
virall <virall.h@gmail.com>
2013-10-14 16:45:02 -0400

    despair

---
+++ EpicorIntegration/Epic_Master.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace EpicorIntegration
+{
+    partial class Epic_Master
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Plants");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Revisions");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Description");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Item", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3});
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            this.splitContainer1.Size = new System.Drawing.Size(775, 574);
+            this.splitContainer1.SplitterDistance = 176;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.FullRowSelect = true;
+            this.treeView1.HotTracking = true;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Plants";
+            treeNode1.Text = "Plants";
+            treeNode2.Name = "Item_Rev";
+            treeNode2.Text = "Revisions";
+            treeNode3.Name = "DescriptionNode";
+            treeNode3.Text = "Description";
+            treeNode4.Name = "ItemNode";
+            treeNode4.Text = "Item";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode4});
+            this.treeView1.Size = new System.Drawing.Size(176, 574);
+            this.treeView1.TabIndex = 1;
+            // 
+            // Epic_Master
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 574);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Epic_Master";
+            this.Text = "Epic_Master";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
---
+++ EpicorIntegration/Item_Ctrl.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace EpicorIntegration
+{
+    partial class Item_Ctrl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.qtybearing = new System.Windows.Forms.CheckBox();
+            this.userevision = new System.Windows.Forms.CheckBox();
+            this.trackserial = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Partnumber_txt = new System.Windows.Forms.TextBox();
+            this.uomweight_cbo = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Description_txt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.type_cbo = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.NetWeight = new System.Windows.Forms.NumericUpDown();
+            this.label9 = new System.Windows.Forms.Label();
+            this.uom_cbo = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.uomclass_cbo = new System.Windows.Forms.ComboBox();
+            this.group_cbo = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.class_cbo = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NetWeight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.qtybearing);
+            this.groupBox1.Controls.Add(this.userevision);
+            this.groupBox1.Controls.Add(this.trackserial);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.Partnumber_txt);
+            this.groupBox1.Controls.Add(this.uomweight_cbo);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.Description_txt);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.type_cbo);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.NetWeight);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.uom_cbo);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.uomclass_cbo);
+            this.groupBox1.Controls.Add(this.group_cbo);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.class_cbo);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(353, 300);
+            this.groupBox1.TabIndex = 34;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Detail";
+            // 
+            // qtybearing
+            // 
+            this.qtybearing.AutoSize = true;
+            this.qtybearing.Location = new System.Drawing.Point(224, 145);
+            this.qtybearing.Name = "qtybearing";
+            this.qtybearing.Size = new System.Drawing.Size(81, 17);
+            this.qtybearing.TabIndex = 34;
+            this.qtybearing.Text = "Qty Bearing";
+            this.qtybearing.UseVisualStyleBackColor = true;
+            // 
+            // userevision
+            // 
+            this.userevision.AutoSize = true;
+            this.userevision.Location = new System.Drawing.Point(224, 168);
+            this.userevision.Name = "userevision";
+            this.userevision.Size = new System.Drawing.Size(89, 17);
+            this.userevision.TabIndex = 33;
+            this.userevision.Text = "Use Revision";
+            this.userevision.UseVisualStyleBackColor = true;
+            // 
+            // trackserial
+            // 
+            this.trackserial.AutoSize = true;
+            this.trackserial.Location = new System.Drawing.Point(224, 191);
+            this.trackserial.Name = "trackserial";
+            this.trackserial.Size = new System.Drawing.Size(123, 17);
+            this.trackserial.TabIndex = 32;
+            this.trackserial.Text = "Track Serial Number";
+            this.trackserial.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Part Number:";
+            // 
+            // Partnumber_txt
+            // 
+            this.Partnumber_txt.Location = new System.Drawing.Point(9, 32);
+            this.Partnumber_txt.Name = "Partnumber_txt";
+            this.Partnumber_txt.Size = new System.Drawing.Size(333, 20);
+            this.Partnumber_txt.TabIndex = 1;
+            // 
+            // uomweight_cbo
+            // 
+            this.uomweight_cbo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.uomweight_cbo.FormattingEnabled = true;
+            this.uomweight_cbo.Location = new System.Drawing.Point(117, 149);
+            this.uomweight_cbo.Name = "uomweight_cbo";
+            this.uomweight_cbo.Size = new System.Drawing.Size(72, 21);
+            this.uomweight_cbo.TabIndex = 31;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Description:";
+            // 
+            // Description_txt
+            // 
+            this.Description_txt.Location = new System.Drawing.Point(9, 71);
+            this.Description_txt.Name = "Description_txt";
+            this.Description_txt.Size = new System.Drawing.Size(333, 20);
+            this.Description_txt.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Type:";
+            // 
+            // type_cbo
+            // 
+            this.type_cbo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.type_cbo.FormattingEnabled = true;
+            this.type_cbo.Location = new System.Drawing.Point(9, 110);
+            this.type_cbo.Name = "type_cbo";
+            this.type_cbo.Size = new System.Drawing.Size(157, 21);
+            this.type_cbo.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Unit Net Weight";
+            // 
+            // NetWeight
+            // 
+            this.NetWeight.DecimalPlaces = 2;
+            this.NetWeight.Location = new System.Drawing.Point(9, 150);
+            this.NetWeight.Maximum = new decimal(new int[] {
+            1410065407,
+            2,
+            0,
+            131072});
+            this.NetWeight.Name = "NetWeight";
+            this.NetWeight.Size = new System.Drawing.Size(102, 20);
+            this.NetWeight.TabIndex = 13;
+            this.NetWeight.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(5, 173);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(82, 13);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "Inventory UOM:";
+            // 
+            // uom_cbo
+            // 
+            this.uom_cbo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.uom_cbo.FormattingEnabled = true;
+            this.uom_cbo.Items.AddRange(new object[] {
+            "Purchased",
+            "Manufacturer",
+            "Sales Kit"});
+            this.uom_cbo.Location = new System.Drawing.Point(8, 189);
+            this.uom_cbo.Name = "uom_cbo";
+            this.uom_cbo.Size = new System.Drawing.Size(180, 21);
+            this.uom_cbo.TabIndex = 23;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 213);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Group:";
+            // 
+            // uomclass_cbo
+            // 
+            this.uomclass_cbo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.uomclass_cbo.FormattingEnabled = true;
+            this.uomclass_cbo.Items.AddRange(new object[] {
+            "Purchased",
+            "Manufacturer",
+            "Sales Kit"});
+            this.uomclass_cbo.Location = new System.Drawing.Point(172, 110);
+            this.uomclass_cbo.Name = "uomclass_cbo";
+            this.uomclass_cbo.Size = new System.Drawing.Size(157, 21);
+            this.uomclass_cbo.TabIndex = 21;
+            this.uomclass_cbo.Visible = false;
+            this.uomclass_cbo.SelectedIndexChanged += new System.EventHandler(this.uomclass_cbo_SelectedIndexChanged);
+            // 
+            // group_cbo
+            // 
+            this.group_cbo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.group_cbo.FormattingEnabled = true;
+            this.group_cbo.Items.AddRange(new object[] {
+            "Purchased",
+            "Manufacturer",
+            "Sales Kit"});
+            this.group_cbo.Location = new System.Drawing.Point(8, 229);
+            this.group_cbo.Name = "group_cbo";
+            this.group_cbo.Size = new System.Drawing.Size(210, 21);
+            this.group_cbo.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(169, 94);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "UOM Class:";
+            this.label8.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(5, 253);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Class:";
+            // 
+            // class_cbo
+            // 
+            this.class_cbo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.class_cbo.FormattingEnabled = true;
+            this.class_cbo.Items.AddRange(new object[] {
+            "Purchased",
+            "Manufacturer",
+            "Sales Kit"});
+            this.class_cbo.Location = new System.Drawing.Point(8, 269);
+            this.class_cbo.Name = "class_cbo";
+            this.class_cbo.Size = new System.Drawing.Size(210, 21);
+            this.class_cbo.TabIndex = 19;
+            // 
+            // Item_Ctrl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Item_Ctrl";
+            this.Size = new System.Drawing.Size(363, 310);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NetWeight)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox qtybearing;
+        private System.Windows.Forms.CheckBox userevision;
+        private System.Windows.Forms.CheckBox trackserial;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Partnumber_txt;
+        private System.Windows.Forms.ComboBox uomweight_cbo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Description_txt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox type_cbo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown NetWeight;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox uom_cbo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox uomclass_cbo;
+        private System.Windows.Forms.ComboBox group_cbo;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox class_cbo;
+    }
+}
